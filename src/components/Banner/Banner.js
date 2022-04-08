0ea9508f4ae5d4c292c2c6263d0352ffa58b4861
nikhea <imonikheaugbodaga@gmail.com>
2022-04-09 00:07:57 +0100

    added images assest

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,4 +1,7 @@
+import Image from "next/image"
+import BannerMain from "../../assets/images/BannerMain.jpg"
 import styles from "./Banner.module.css";
+
 const Banner = () => {
   return <div className={styles.banner}>Banner</div>;
 };
